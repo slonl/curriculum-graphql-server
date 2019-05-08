21aa6ee0d1593d5e5d1ba4d19d4739ec8eaf26ef
Auke van Slooten <auke@muze.nl>
2019-05-08 10:16:48 +0200

    updating data, added data types to combine.js

--- data/combine.js
+++ data/combine.js
@@ -1,5 +1,7 @@
-	var curriculum     = require('./curriculum-doelen/lib/curriculum.js');
-	var doelenSchema   = curriculum.loadSchema('./curriculum-doelen/context.json', './curriculum-doelen/')
+	var curriculum	 = require('./curriculum-doelen/lib/curriculum.js');
+	var doelenSchema   = curriculum.loadSchema('./curriculum-doelen/context.json', './curriculum-doelen/');
+	var kerndoelenSchema   = curriculum.loadSchema('./curriculum-kerndoelen/context.json', './curriculum-kerndoelen/')
+	var leerdoelenkaartenSchema = curriculum.loadSchema('./curriculum-leerdoelenkaarten/context.json', './curriculum-leerdoelenkaarten/');
 	var inhoudenSchema = curriculum.loadSchema('./curriculum-inhouden/context.json', './curriculum-inhouden/');
 
 	//FIXME: alias has 'parent_id', so data.parent is needed for json-graphql-server
@@ -11,100 +13,122 @@
 	var reverseNiveauIndex = {};
 	function makeIndex() {
 
-		var types = ['vak','vakkern','vaksubkern','vakinhoud','doelniveau','doel','niveau'];
-
+		// FIXME: read this from the json context instead of hard-coding it.
+		var types = [
+			// Inhouden
+			'vak','vakkern','vaksubkern','vakinhoud',
+			// Doelen
+			'doelniveau','doel','niveau',
+			// Leerdoelenkaarten
+			'ldk_vak','ldk_vakkern','ldk_vaksubkern','ldk_vakinhoud',
+			// Kerndoelen
+			'kerndoel','kerndoel_domein','kerndoel_vakleergebied','kerndoel_uitstroomprofiel'
+		];
+		
 		types.forEach(function(section) {
-	        curriculum.data[section].forEach(function(entity) {
-	            idIndex[entity.id] = Object.assign({ section: section, parents: [] },entity);
-	        });
-	    });
+			curriculum.data[section].forEach(function(entity) {
+				idIndex[entity.id] = Object.assign({ section: section, parents: [] },entity);
+			});
+		});
 
-	    Object.keys(idIndex).forEach(function(id) {
-	        var entity = idIndex[id];
-	        var parentTypes = types.slice();
+		Object.keys(idIndex).forEach(function(id) {
+			var entity = idIndex[id];
+			var parentTypes = types.slice();
 			parentTypes.pop();
 			parentTypes.forEach(function(section) {
-	            if (typeof entity[section+'_id'] != 'undefined') {
-	                entity[section+'_id'].forEach(function(childId) {
-	                    if (typeof idIndex[childId] == 'undefined') {
-	                        console.log('missing '+childId+' in '+section, entity);
-	                    }
-	                    idIndex[childId].parents.push(id);
-	                });
-	            }
-	            if (typeof entity['doelniveau_id'] != 'undefined') {
-	                entity['doelniveau_id'].forEach(function(childId) {
-	                    if (typeof idIndex[childId] == 'undefined') {
-	                        console.log('missing '+childId+' in '+section, entity);
-	                    }
-	                    idIndex[childId].parents.push(id);
-	                });
-	            }
-	        });
-	    });
+				if (typeof entity[section+'_id'] != 'undefined') {
+					if (Array.isArray(entity[section+'_id'])) {
+						entity[section+'_id'].forEach(function(childId) {
+							if (typeof idIndex[childId] == 'undefined') {
+								console.log('missing '+childId+' in '+section, entity);
+							}
+							idIndex[childId].parents.push(id);
+						});
+					} else {
+						if (typeof idIndex[entity[section+'_id']] != 'undefined') {
+							idIndex[entity[section+'_id']].parents.push(id);
+						}
+					}
+				}
+				if (typeof entity['doelniveau_id'] != 'undefined') {
+					entity['doelniveau_id'].forEach(function(childId) {
+						if (typeof idIndex[childId] == 'undefined') {
+							console.log('missing '+childId+' in '+section, entity);
+						}
+						idIndex[childId].parents.push(id);
+					});
+				}
+			});
+		});
 
-	    function getNiveauIndex(niveauId) {
-	        var niveauOb = niveauIndex.filter(function(niveauOb) {
-	            return niveauOb.niveau_id == niveauId;
-	        }).pop();
-	        if (!niveauOb) {
-	            niveauOb = {
-	                niveau_id: niveauId,
-	                vak_id: [],
-	                vakkern_id: [],
-	                vaksubkern_id: [],
-	                vakinhoud_id: [],
+		function getNiveauIndex(niveauId) {
+			var niveauOb = niveauIndex.filter(function(niveauOb) {
+				return niveauOb.niveau_id == niveauId;
+			}).pop();
+			if (!niveauOb) {
+				niveauOb = {
+					niveau_id: niveauId,
+					vak_id: [],
+					vakkern_id: [],
+					vaksubkern_id: [],
+					vakinhoud_id: [],
+					ldk_vak_id: [],
+					ldk_vakkern_id: [],
+					ldk_vaksubkern_id: [],
+					ldk_vakinhoud_id: [],
 					doel_id: []
-	            };
-	            niveauIndex.push(niveauOb);
-	        }
-	        return niveauOb;
-	    }
-
-	    function getParentType(entity) {
-	        var types=['vak','vakkern','vaksubkern','vakinhoud'];
-	        var index=types.indexOf(entity.section);
-	        if (index>0) {
-	            return types[index-1];
-	        }
+				};
+				niveauIndex.push(niveauOb);
+			}
+			return niveauOb;
 		}
 
-	    function addParentsToNiveauIndex(parents, niveaus) {
-	        niveaus.forEach(function(niveauId) {
-	            var niveau = getNiveauIndex(niveauId);
-	            parents.forEach(function(parentId) {
-	                var parent = idIndex[parentId];
-	                    if (niveau[parent.section+'_id'].indexOf(parentId)==-1) {
-	                        niveau[parent.section+'_id'].push(parentId);
-	                    }
-	                    if (typeof parent.parents != 'undefined') {
-	                        addParentsToNiveauIndex(parent.parents, niveaus);
-	                    }
-	            });
-	        });
-	        parents.forEach(function(parentId) {
-	        	if (!reverseNiveauIndex[parentId]) {
-	        		reverseNiveauIndex[parentId] = [];
-	        	}
-	        	reverseNiveauIndex[parentId] = reverseNiveauIndex[parentId].concat(niveaus).filter(onlyUnique);
-	        });
-	    }
+		function getParentType(entity) {
+			var types=['vak','vakkern','vaksubkern','vakinhoud'];
+			var index=types.indexOf(entity.section);
+			if (index>0) {
+				return types[index-1];
+			}
+		}
+
+		function addParentsToNiveauIndex(parents, niveaus) {
+			niveaus.forEach(function(niveauId) {
+				var niveau = getNiveauIndex(niveauId);
+				parents.forEach(function(parentId) {
+					var parent = idIndex[parentId];
+					if (Array.isArray(niveau[parent.section+'_id'])) {
+						if (niveau[parent.section+'_id'].indexOf(parentId)==-1) {
+							niveau[parent.section+'_id'].push(parentId);
+						}
+						if (typeof parent.parents != 'undefined') {
+							addParentsToNiveauIndex(parent.parents, niveaus);
+						}
+					}
+				});
+			});
+			parents.forEach(function(parentId) {
+				if (!reverseNiveauIndex[parentId]) {
+					reverseNiveauIndex[parentId] = [];
+				}
+				reverseNiveauIndex[parentId] = reverseNiveauIndex[parentId].concat(niveaus).filter(onlyUnique);
+			});
+		}
 
 		function onlyUnique(value, index, self) {
 			return self.indexOf(value)===index;
 		}
 
-	    var count = 0;
-	    var error = 0;
-	    curriculum.data.doelniveau.forEach(function(doelniveau) {
-	    	var parents = idIndex[doelniveau.id].parents;
-	    	if (!parents) {
-	    		console.log('missing doelniveau parents for '+doelniveau.id);
-	    		error++;
-	    		return;
-	    	}
-	    	count++;
-	        addParentsToNiveauIndex(parents, doelniveau.niveau_id);
+		var count = 0;
+		var error = 0;
+		curriculum.data.doelniveau.forEach(function(doelniveau) {
+			var parents = idIndex[doelniveau.id].parents;
+			if (!parents) {
+				console.log('missing doelniveau parents for '+doelniveau.id);
+				error++;
+				return;
+			}
+			count++;
+			addParentsToNiveauIndex(parents, doelniveau.niveau_id);
 			doelniveau.niveau_id.forEach(function(niveauId) {
 				var index = getNiveauIndex(niveauId);
 				doelniveau.doel_id.forEach(function(doelId) {
@@ -113,8 +137,8 @@
 					}
 				});
 			});
-	    });
-	    console.log(count+' correct, '+error+' errors');
+		});
+		console.log(count+' correct, '+error+' errors');
 	}
 
 
@@ -123,5 +147,27 @@
 	makeIndex();
 	curriculum.data.niveauIndex = niveauIndex;
 	
-	var fileData = JSON.stringify(curriculum.data, null, "\t");
+	var combined = JSON.parse(JSON.stringify((curriculum)));
+	
+	for (i in combined.data) {
+		combined.data[i].forEach(function(entry, index) {
+			var fields = ["vak_id", "vakkern_id", "vaksubkern_id", "vakinhoud_id"];
+			fields.forEach(function(field) {
+				if (entry[field] && !Array.isArray(entry[field])) {
+					combined.data[i][index][field] = [entry[field]];
+				}
+			});
+		});
+	}
+
+	for (i in combined.data) {
+		var fields = ["deprecated", "ldk_deprecated"];
+		if (fields.indexOf(i) !== 0) {
+			if (Array.isArray(combined.data[i]) && combined.data[i].length === 0) {
+				delete combined.data[i];
+			}
+		}
+	}
+
+	var fileData = JSON.stringify(combined.data, null, "\t");
 	fs.writeFileSync('./combined.json', fileData);
