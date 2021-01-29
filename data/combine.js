93b51072b34beca26723dd5b29866a2713f2df61
Auke van Slooten <auke@muze.nl>
2021-01-29 15:42:53 +0100

    updated to 2020.2 release

--- data/combine.js
+++ data/combine.js
@@ -8,7 +8,8 @@
 	var examenprogrammaBgSchema = curriculum.loadSchema('./curriculum-examenprogramma-bg/context.json', './curriculum-examenprogramma-bg/');
 	var doelgroeptekstenSchema  = curriculum.loadSchema('./curriculum-doelgroepteksten/context.json', './curriculum-doelgroepteksten/');
 	var syllabusSchema          = curriculum.loadSchema('./curriculum-syllabus/context.json', './curriculum-syllabus/');
-
+	var inhoudslijnenSchema     = curriculum.loadSchema('./curriculum-inhoudslijnen/context.json', './curriculum-inhoudslijnen/');
+		
 	//FIXME: alias has 'parent_id', so data.parent is needed for json-graphql-server
 	curriculum.data.parent = [{id:null}];
 
@@ -21,9 +22,9 @@
 		// FIXME: read this from the json context instead of hard-coding it.
 		var types = [
 			// Leerdoelenkaarten
-			'ldk_vak','ldk_vakkern','ldk_vaksubkern','ldk_vakinhoud',
+			'ldk_vakleergebied','ldk_vakkern','ldk_vaksubkern','ldk_vakinhoud',
 			// Inhouden
-			'lpib_vakkern','lpib_vaksubkern','lpib_vakinhoud',
+			'lpib_vakleergebied', 'lpib_vakkern','lpib_vaksubkern','lpib_vakinhoud',
 			// Doelen
 			'doelniveau','doel','niveau','vakleergebied',
 			// Kerndoelen
@@ -41,19 +42,23 @@
 			// leerplan in beeld
 			'lpib_vakkencluster','lpib_leerlijn',
 			//syllabus
-			'syllabus', 'syllabus_specifieke_eindterm', 'syllabus_toelichting', 'syllabus_vakbegrip'
+			'syllabus', 'syllabus_specifieke_eindterm', 'syllabus_toelichting', 'syllabus_vakbegrip',
+			// inhoudslijnen
+			'inh_vakleergebied', 'inh_cluster', 'inh_subcluster', 'inh_inhoudslijn'
 		];
 
 		// ignore related links that aren't parent-child relations		
 		var ignore = {
-			'ldk_vak': ['vakleergebied_id'],
+			'ldk_vakleergebied': ['vakleergebied_id'],
 			'ldk_vakkern': ['lpib_vakkern_id'],
 			'ldk_vaksubkern': ['lpib_vaksubkern_id'],
 			'ldk_vakinhoud': ['lpib_vakinhoud_id'],
 			'kerndoel_vakleergebied': ['vakleergebied_id'],
 			'examenprogramma_vakleergebied': ['vakleergebied_id'],
 			'lpib_leerlijn': ['vakleergebied_id', 'lpib_vakinhoud_id'],
-			'lpib_vakkencluster': ['vakleergebied_id']
+			'lpib_vakkencluster': ['vakleergebied_id'],
+			'lpib_vakleergebied': ['vakleergebied_id'],
+			'inh_vakleergebied': ['vakleergebied_id']
 		};
 		
 		function shouldIgnore(section, property) {
@@ -167,7 +172,10 @@
 					syllabus_specifieke_eindterm_id: [],
 					syllabus_toelichting_id: [],
 					syllabus_vakbegrip_id: [],
-					syllabus_id: []
+					syllabus_id: [],
+					inh_vakleergebied_id: [],
+					inh_inhoudslijn_id: [],
+					inh_cluster_id: []
 				};
 				niveauIndex.push(niveauOb);
 			}
